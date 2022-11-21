0c46c0c145b1eca05aa685fa104667dbb76515d2
kirkmonz <kirkemmons2@gmail.com>
2022-11-20 21:50:15 -0500

    update show option for modal github button portfolio section

--- src/components/helpers/Modal.vue
+++ src/components/helpers/Modal.vue
@@ -62,10 +62,18 @@
               class="mt-1 mb-3"
               :class="{ pgray: !nightMode, 'bg-secondary': nightMode }"
             />
-            <button class="btn w-25 mr-3" @click="open(portfolio.github)">
-              github
-            </button>
-            <button class="btn w-25" @click="$emit('close')">close</button>
+            
+            <button 
+              class="btn w-25 mr-3"
+              v-if="portfolio.github"
+              @click="open(portfolio.github)"
+              >github</button>
+
+            <button 
+              class="btn w-25" 
+              @click="$emit('close')"
+              >close</button>
+
           </div>
         </div>
       </div>
